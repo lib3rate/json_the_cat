59dd524d211113ccbcc4ff2f873db98650772f46
Dmitry Petrov <e.liberate@gmail.com>
2020-06-04 22:25:53 +0000

    Added tests

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,17 @@
 const request = require('request');
 
-const breed = process.argv[2];
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      error = "Cannot find the specified breed :(";
+      const desc = null;
+      callback(error, desc);
+    } else {
+      const desc = data[0].description;
+      callback(error, desc);
+    }
+  });
+};
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log(error);
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log("Cannot find the specified breed :(");
-    return;
-  }
-  console.log(data[0].description);
-});
+module.exports = { fetchBreedDescription }
